360dd9bf395dd8893fd2fd0a752b086a2e0cf2ac
phageghost <3150660+phageghost@users.noreply.github.com>
2025-05-05 14:27:19 -0700

    Refactored for clarity with identrical logic

---
+++ constants.py
@@ -0,0 +1,8 @@
+TRAIN_FASTA_FPATH = "cafa-5-protein-function-prediction/Train/train_sequences.fasta"
+TRAIN_TERMS_FPATH = "cafa-5-protein-function-prediction/Train/train_terms.tsv"
+
+TEST_FASTA_FPATH = "cafa-5-protein-function-prediction/Test (Targets)/testsuperset.fasta"
+OBO_FPATH = "cafa-5-protein-function-prediction/Train/go-basic.obo"  
+
+MODEL_ID = "AmelieSchreiber/esm2_t6_8M_UR50D"  # Replace with your Hugging Face hub model name
+MINIMUM_PROBABILITY = 0.05
