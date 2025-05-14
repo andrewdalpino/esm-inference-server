19e58975d719a7fd2db0f746ba39a6cf7f70d92a
Andrew DalPino <andrewdalpino@icloud.com>
2025-05-13 20:16:12 -0500

    Add API token authentication

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from fastapi import FastAPI, HTTPException
 
 from model import ESMClassifier
 
-from http_types import  HealthResponse, PredictRankRequest, PredictRankResponse
+from http_types import HealthResponse, PredictRankRequest, PredictRankResponse
 
-# General environment variables for model configuration.
+from fastapi.responses import JSONResponse
+
+api_token = environ.get("API_TOKEN", "")
 model_name = environ.get("MODEL_NAME", "facebook/esm2_t6_8M_UR50D")
 context_length = int(environ.get("CONTEXT_LENGTH", 1024))
 device = environ.get("DEVICE", "cpu")
-
 
 app = FastAPI(
     title="ESM Inference API",
@@ -21,6 +22,33 @@ app = FastAPI(
 )
 
 model = ESMClassifier(model_name, context_length, device)
+
+
+@app.middleware("http")
+async def exception_handler(request, call_next):
+    """Handle exceptions emitted from the domain model."""
+
+    try:
+        return await call_next(request)
+    except HTTPException as e:
+        return JSONResponse(content={"message": e.detail}, status_code=e.status_code)
+    except Exception as e:
+        return JSONResponse(
+            content={"message": "Something went wrong."}, status_code=500
+        )
+
+
+@app.middleware("http")
+async def authorization(request, call_next):
+    """Check for the API token in the request headers."""
+
+    if api_token:
+        token = request.headers.get("Authorization")
+
+        if not token or token != f"Bearer {api_token}":
+            return JSONResponse(content={"message": "Unauthorized."}, status_code=401)
+
+    return await call_next(request)
 
 
 @app.get("/health", response_model=HealthResponse)
@@ -45,4 +73,4 @@ async def predict_rank(request: PredictRankRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
